a53a8e79d95cb48d08f9077802ac1e8ab3084bd9
dalwrigh <dalwrigh@cisco.com>
2024-06-26 08:25:39 -0400

    added some testing scenarios and examples

--- examples/scratch.py
+++ examples/scratch.py
@@ -1,37 +1,10 @@
 import json
 from json_parser import JsonParser
 
-json_data = [
-    {
-        "first_name": "John",
-        "last_name": "Doe",
-        "full_name": "John Doe",
-        "address1": {
-            "street": "1208 Elm Street",
-            "city": "Springfield",
-            "zip_code": "62704"
-        },
-        "address2": {
-            "street": "4965 Harvest Rd",
-            "city": "Springfield",
-            "zip_code": "62704"
-        },
-        "birthday": "1984-05-23"
-    },
-    {
-        "first_name": "Jane",
-        "last_name": "Smith",
-        "full_name": "Jane Smith",
-        "address": {
-            "street": "742 Evergreen Terrace",
-            "city": "Springfield",
-            "zip_code": "62701"
-        },
-        "birthday": "1990-11-12"
-    }
-]
+with open("bakery.json", "r") as f:
+    json_data = json.load(f)
 
-keys = ['first_name', 'last_name', 'birthday']
+keys = ['name', 'batter', 'topping']
 
 data = JsonParser(json_data, keys)
 result = data.get_data()
